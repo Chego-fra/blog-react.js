a006d633e17951c921f2d7221c9a23d6532c8d08
Francis Chege <chegefrancis664@gmail.com>
2024-08-28 13:48:51 +0300

    created navbar and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Create from './components/create';
+// import Home from './components/home';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar/>
+      {/* <Home/> */}
+      <Create/>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () =>{
+    return(
+        <nav>
+            <div  className="navbar">
+            <h1>Blog Post</h1>
+                <div className="links">
+                <a href="#">Home</a>
+                <a href="#">Create</a>
+                </div>
+            </div>
+            <hr/>
+        </nav>
+
+    )
+}
+export default Navbar;
